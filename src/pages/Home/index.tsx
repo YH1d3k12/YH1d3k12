62bfe0649c087a4142d796e2e5bea7a22d077a34
Hideki <djalmahy@hotmail.com>
2025-02-17 21:58:06 -0300

    Feat: added github and linkedin icons to title.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,5 @@
+import { IoLogoGithub, IoLogoLinkedin } from 'react-icons/io5';
+import { Link } from 'react-router-dom';
 import './styles.css';
 
 function Home() {
@@ -6,6 +8,14 @@ function Home() {
             <div className="home-title">
                 <h1>Djalma Hideki Yamamoto</h1>
                 <p>Fullstack Developer</p>
+                <div className="home-midia-links">
+                    <Link to="https://github.com/YH1d3k12">
+                        <IoLogoGithub />
+                    </Link>
+                    <Link to="https://br.linkedin.com/in/yamamoto-hideki">
+                        <IoLogoLinkedin />
+                    </Link>
+                </div>
             </div>
         </div>
     );
