b4ba6c474f41aeffc71f3d0127e9e23f9e158d42
Djalma <djalmahy@hotmail.com>
2025-03-25 21:25:50 -0300

    feat: added gradient over image.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,13 @@
 import { IoLogoGithub, IoLogoLinkedin } from 'react-icons/io5';
 import { Link } from 'react-router-dom';
+import backgroundImage from '../../assets/city.jpg';
 import './styles.css';
 
 function Home() {
     return (
         <div className="page-screen home-page">
-            <div className="home-background"></div>
+            <img className="home-background-image" src={backgroundImage} />
+            <div className="home-gradient"></div>
             <div className="home-title">
                 <h1>Djalma Hideki Yamamoto</h1>
                 <p>Fullstack Developer</p>
