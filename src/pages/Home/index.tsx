851693545709f49105416c2f63a8d92cd13505c6
Djalma <djalmahy@hotmail.com>
2025-03-26 21:48:17 -0300

    WIP: About section

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,12 @@
 import Hero from './hero';
+import About from './about';
 import './styles.css';
 
 function Home() {
     return (
         <div className="home-page">
             <Hero />
+            <About />
         </div>
     );
 }
