8d8dc9955a4052e5eefc68c7bae8d95cd87a07f3
Djalma <djalmahy@hotmail.com>
2025-04-11 21:07:10 -0300

    Feat: created slider.

--- src/pages/Home/about.tsx
+++ src/pages/Home/about.tsx
@@ -1,7 +1,12 @@
 import profilePicture from '../../assets/profile.jpg';
+import Slider from '../../components/Slider';
 
 function About() {
-    return <section className="page-screen about-section"></section>;
+    return (
+        <section className="page-screen about-section">
+            <Slider />
+        </section>
+    );
 }
 
 export default About;
