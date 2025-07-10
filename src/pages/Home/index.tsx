a2c093853233e68613bf4649fa53f3ab9d90c77c
Djalma <djalmahy@hotmail.com>
2025-07-10 14:01:12 -0300

    Chore: removed components from this project.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,7 @@
-import Hero from '../CityLandscape';
-import About from './about';
 import './styles.css';
 
 function Home() {
-    return (
-        <main className="home-page">
-            <Hero />
-            <About />
-        </main>
-    );
+    return <main className="home-page"></main>;
 }
 
 export default Home;
