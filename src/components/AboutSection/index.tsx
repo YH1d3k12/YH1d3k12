222672ce61a8d8162a64f05d6ff58f20401dac14
Djalma <djalmahy@hotmail.com>
2025-07-17 19:38:55 -0300

    Feat: Created about section.

---
+++ src/components/AboutSection/index.tsx
@@ -0,0 +1,26 @@
+import photo from '../../assets/photo.jpg';
+import './styles.css';
+
+export default function AboutSection() {
+    return (
+        <section className="about-section">
+            <div className="about-section-text">
+                <h1>I'm Djalma</h1>
+                <h2>Fullstack Developer</h2>
+                <p>
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a
+                    type specimen book. It has survived not only five centuries,
+                    but also the leap into electronic typesetting, remaining
+                    essentially unchanged.
+                </p>
+                <p>Joinville, SC - Brazil</p>
+            </div>
+            <div className="about-section-img">
+                <img src={photo} alt="" />
+            </div>
+        </section>
+    );
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,27 +1,10 @@
-import sampleImg from '../../assets/ct.png';
+import AboutSection from '../../components/AboutSection';
 import './styles.css';
 
 function Home() {
     return (
         <main className="home-page page-padding">
-            <section className="home-hero">
-                <div className="home-hero-text">
-                    <h1>I'm Djalma</h1>
-                    <h2>Fullstack Developer</h2>
-                    <p>
-                        Lorem Ipsum is simply dummy text of the printing and
-                        typesetting industry. Lorem Ipsum has been the
-                        industry's standard dummy text ever since the 1500s,
-                        when an unknown printer took a galley of type and
-                        scrambled it to make a type specimen book. It has
-                        survived not only five centuries, but also the leap into
-                        electronic typesetting, remaining essentially unchanged.
-                    </p>
-                </div>
-                <div className="home-hero-img">
-                    <img src={sampleImg} alt="" />
-                </div>
-            </section>
+            <AboutSection />
         </main>
     );
 }
