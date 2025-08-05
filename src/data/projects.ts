4b9c933127d1acc4854ecf06dc718c1035c056f1
Djalma <djalmahy@hotmail.com>
2025-08-05 14:06:57 -0300

    Chore: added icons project to portfolio.

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,6 +3,10 @@ import powebPortrait from '../assets/gallery/poweb.jpeg';
 import powebA from '../assets/gallery/powebA.jpeg';
 import powebB from '../assets/gallery/powebB.jpeg';
 import powebC from '../assets/gallery/powebC.jpeg';
+import icsPortrait from '../assets/gallery/ICS.png';
+import icsA from '../assets/gallery/ICSA.png';
+import icsB from '../assets/gallery/ICSB.png';
+import icsC from '../assets/gallery/ICSC.png';
 
 const projects: Project[] = [
     {
@@ -21,6 +25,22 @@ const projects: Project[] = [
         githubUrl: '',
         liveUrl: '',
         gallery: [powebA, powebB, powebC],
+    },
+    {
+        id: 2,
+        title: 'projects.iconsCharacterSheet.title',
+        description: 'projects.iconsCharacterSheet.description',
+        image: icsPortrait,
+        tags: ['React', 'TypeScript'],
+        detailedDescription: 'projects.iconsCharacterSheet.detailedDescription',
+        features: [
+            'projects.iconsCharacterSheet.features.characterContext',
+            'projects.iconsCharacterSheet.features.attributeCalculation',
+            'projects.iconsCharacterSheet.features.characterFile',
+        ],
+        githubUrl: 'https://github.com/YH1d3k12/icons-character-sheet',
+        liveUrl: 'https://yh1d3k12.github.io/icons-character-sheet/',
+        gallery: [icsA, icsB, icsC],
     },
 ];
 
